2683905cd3839b98125eec4c7c2156daf1a0e87a
erikgett <erikgette63@gmail.com>
2023-04-28 14:17:15 +0500

    murun.py как раз запускает мое приложение в ынтырнете

--- test_flask_and all/myrun.py
+++ test_flask_and all/myrun.py
@@ -3,14 +3,17 @@ from werkzeug.serving import run_simple
 
 from app1 import app as app1
 from app2 import app as app2
-
 from flask_app import flask_app
+from flask import Flask
+
 from flask_ngrok import run_with_ngrok
-run_with_ngrok(flask_app)
-application = DispatcherMiddleware(flask_app, {
+
+application = Flask(__name__)
+application.wsgi_app = DispatcherMiddleware(flask_app, {
     '/app1': app1.server,
     '/app2': app2.server,
 })
+run_with_ngrok(application)
 
-if __name__ == "__main__":
-    flask_app.run()
+if __name__ == '__main__':
+    application.run()
--- test_flask_and all/run.py
+++ test_flask_and all/run.py
@@ -3,14 +3,12 @@ from werkzeug.serving import run_simple
 
 from app1 import app as app1
 from app2 import app as app2
-
 from flask_app import flask_app
-from flask_ngrok import run_with_ngrok
-run_with_ngrok(flask_app)
+
 application = DispatcherMiddleware(flask_app, {
     '/app1': app1.server,
     '/app2': app2.server,
 })
 
-if __name__ == "__main__":
-    flask_app.run()
+if __name__ == '__main__':
+    run_simple('localhost', 8050, application)
