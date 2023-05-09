e4ca676a5118e3c82b8be1e0790c1f1ea24a20f9
erikgett <erikgette63@gmail.com>
2023-05-09 14:45:43 +0500

    добавлена первая страница

--- MainPage.py
+++ MainPage.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 flask_app = Flask(__name__)
+from DashBoads import statisticsPage as dash_app
 
 
 @flask_app.route('/')
 def index():
-    return 'Hello Flask app'
+    return render_template('index.html')
+
+
+@flask_app.route('/dash')
+def dash_page():
+    return dash_app.index()
