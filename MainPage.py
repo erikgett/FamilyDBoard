880c6cb500b6b2e6b40630ec4f30fa9e422032c6
erikgett <erikgette63@gmail.com>
2023-05-09 23:15:54 +0500

    Добавили лого

--- MainPage.py
+++ MainPage.py
@@ -1,17 +1,36 @@
 from flask import Flask, url_for, render_template
 
 flask_app = Flask(__name__)
-from DashBoads import statisticsPage as dash_app
-
+from DashBoads import statisticsPage
+from NewsPage import FamiliesNewsPage
 
 @flask_app.route('/')
 def index():
     return render_template('mainPage.html')
 
-@flask_app.route('/test')
+
+@flask_app.route('/statistics')
+def statistics():
+    return render_template('statisticPage.html')
+
+@flask_app.route('/newsPage')
+def newsPage():
+    return render_template('Families.html')
+
+
+@flask_app.route('/mainFraim')
 def mainFraim():
     return render_template('mainFraim.html')
 
-@flask_app.route('/dash')
+@flask_app.route('/InfoPage')
+def InfoPage():
+    return render_template('InfoPage.html')
+
+
+@flask_app.route('/statisticsPage')
 def dash_page():
-    return dash_app.index()
+    return statisticsPage.index()
+
+@flask_app.route('/FamiliesNewsPage')
+def FamiliesNewsPage():
+    return FamiliesNewsPage.index()
